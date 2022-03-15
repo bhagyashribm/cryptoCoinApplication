558c305cd137a32ee3b63e9f57d3c34b740ad264
bhagyashriibm <bhagyashri.lokhande@ibm.com>
2022-03-15 15:16:59 +0530

    initial commit

--- app/src/main/java/com/example/myapplication/data/remote/CoinPaprikaApi.kt
+++ app/src/main/java/com/example/myapplication/data/remote/CoinPaprikaApi.kt
@@ -7,8 +7,8 @@ import retrofit2.http.*
 
 interface CoinPaprikaApi {
 
-    @Headers("apisign: 026077fa49df05536b00baca986efbda5dd366a219936546023fed2fb36fd5870b241b689e8441e581236b4ef1e7f35c6e5992775a14c362a69d6f02d3278d05", "Accept: application/json",
-        "User-Agent: Your-App-Name",
+    @Headers("apisign: 026077fa49df05536b00baca986efbda5dd366a219936546023fed2fb36fd5870b241b689e8441e581236b4ef1e7f35c6e5992775a14c362a69d6f02d3278d05",
+        "Accept: application/json",
         "Cache-Control: max-age=640000"
     )
     @GET("/api/v3/private/getdepositaddress")
